39fad7b05babb6ada1c86747d91cbc1706a416ea
evermake <vladeryabkin@gmail.com>
2022-04-03 16:56:06 +0300

    create RangeMap interface

---
+++ src/main/java/rangemap/RangeMap.java
@@ -0,0 +1,13 @@
+package rangemap;
+
+import java.util.List;
+
+public interface RangeMap<K, V> {
+  int size();
+  boolean isEmpty();
+  void add(K key, V value);
+  V remove(K key);
+  boolean contains(K key);
+  V lookup(K key);
+  List<V> lookupRange(K from, K to);
+}
