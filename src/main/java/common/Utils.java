5269cd4b872dac47c6c44f39ee246800ea98d4c1
evermake <vladeryabkin@gmail.com>
2022-04-24 20:43:48 +0300

    add comments to Utils

--- src/main/java/common/Utils.java
+++ src/main/java/common/Utils.java
@@ -1,8 +1,23 @@
+/**
+ * Created by Vladislav Deryabkin
+ */
 package common;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Helper class with utils.
+ */
 public class Utils {
+  /**
+   * Returns random integer between a and b.
+   * Required for testing.
+   *
+   * @param a left bound [inclusive]
+   * @param b right bound [inclusive]
+   *
+   * @return random value between a and b
+   */
   public static Integer getRandomInteger(int a, int b) {
     return ThreadLocalRandom.current().nextInt(a, b + 1);
   }
